c865d732040ab6e7ba9590cb5eacc915cccfc64b
GabrielLennox <meunomeelennox@gmail.com>
2021-05-18 19:00:28 -0300

    exercicios de estruturas condicionais, tipos primitivos e operadores logicos

---
+++ 4.1_ex/if-else.js
@@ -0,0 +1,9 @@
+let grade = 100;
+
+if (grade < 60) {
+  console.log('Você foi reprovada(o)');
+} else if (grade < 80) {
+  console.log('Você está na nossa lista de espera');
+} else {
+  console.log ('Parabéns, você foi aprovada(o)!');
+};
---
+++ 4.1_ex/retangulo.js
@@ -0,0 +1,13 @@
+let base;
+let altura;
+
+base = 5;
+altura = 8;
+
+const area = base * altura;
+
+console.log(area);
+
+const perimetro = base + altura + base + altura;
+
+console.log(perimetro);
