2ce9908b526b737d7c2b5d450895cd409fa0c421
GabrielLennox <meunomeelennox@gmail.com>
2021-05-19 15:38:27 -0300

    exercicios do final da primeira parte da introducao a JS e logica de programacao

--- 4.1_ex/primeiros-passos.js
+++ 4.1_ex/primeiros-passos.js
@@ -1,15 +1,48 @@
-const name = "Gabriel";
-const birthCity = "Anapolis";
-let birthYear = 1995;
+let a = 5;
+let b = '5';
 
-console.log(name);
-console.log(birthCity);
-console.log(birthYear);
+// a = 10 && b = '2'
+// 102 --> number + string will concat
+// 8 --> all other operations will work normally with value of numeric string
+// 20
+// 5
+// 0
+// a = 10 && b = undefined
+// NaN --> when either element is undefined all operations result in undefined
+// a = 10 && b = 'xablau'
+// 10xablau --> number + string will concat
+// NaN --> number + text string all other operations will result in NaN
+// a = 'xablau' && b = 5
+// xablau5 --> string plus number will concat
+// NaN --> all other operations will result in NaN
+// a = 5 && b = 0
+// 5
+// 5
+// 0
+// Infinity
+// NaN
 
-birthYear = 2030;
+let sum = a + b;
+console.log(sum);
 
-console.log(birthYear);
+let subtract = a - b;
+console.log(subtract);
 
-birthCity = "Montreal"; // birthCity e uma constante, sendo assim, nao se pode reatribuir seu valor
+let multiply = a * b;
+console.log(multiply);
 
-console.log(birthCity);
+let divide = a / b;
+console.log(divide);
+
+let remainder = a % b;
+console.log(remainder);
+
+if (a > b) {
+  console.log(a);
+} else if (a < b) {
+  console.log(b);
+} else if (a == b) {
+  console.log('The numbers are equal');
+} else {
+  console.log('invalid comparison');
+};
---
+++ 4.1_ex/variaveis.js
@@ -0,0 +1,15 @@
+const name = "Gabriel";
+const birthCity = "Anapolis";
+let birthYear = 1995;
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Montreal"; // birthCity e uma constante, sendo assim, nao se pode reatribuir seu valor
+
+console.log(birthCity);
