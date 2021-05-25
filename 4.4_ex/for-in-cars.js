3c6e360f2fe20bc027f0fdd108c43c749a09b143
GabrielLennox <meunomeelennox@gmail.com>
2021-05-25 17:45:32 -0300

    exemplo do metodo for in

---
+++ 4.4_ex/for-in-cars.js
@@ -0,0 +1,40 @@
+// *EXAMPLE
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars) {
+//   console.log(cars[index]);
+// }
+
+// let car = {
+//   type: 'Fiat',
+//   model: '500',
+//   color: 'white',
+// };
+
+// for (let index in car) {
+//   console.log(index, car[index]);
+// }
+
+// ?Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+};
+
+for (const key in names) {
+  // console.log('Olá', names[key]);
+}
+
+// ?Usando o objeto abaixo, utilize For/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (const keys in car) {
+  console.log(keys, car[keys]);
+}
